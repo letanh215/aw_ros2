69bbb72adc1f93142c2925f8a7f23987715d0a99
letanh215 <letanh215@gmail.com>
2025-04-18 11:08:02 +0800

    Update Dockerfile

--- config.bash
+++ config.bash
@@ -1,10 +1,3 @@
-if [ ! -e /tmp/cycloneDDS_configured ]; then
-    sysctl -w net.core.rmem_max=2147483647
-    sysctl -w net.ipv4.ipfrag_time=3
-    sysctl -w net.ipv4.ipfrag_high_thresh=134217728     # (128 MB)
-    ip link set lo multicast on
-    touch /tmp/cycloneDDS_configured
-fi
 export DISPLAY=:1
 export ROS_DOMAIN_ID=0
 export TURTLEBOT3_MODEL=waffle
